7c2e56fcfe427d4f8cf8173c2e2e35ed0dc9f34e
Keshav2710 <rocky.the.boss.kgf@gmail.com>
2022-09-10 23:38:32 +0530

    added App and Global Static Files

--- MYPAGE/settings.py
+++ MYPAGE/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#For Global Static Files we need to add STATICFILES_DIRS extra setting
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
